2a5981d2eb87d1d8da6dcfa5ca6c027b901ee4fb
Helder1121 <78274380+Helder1121@users.noreply.github.com>
2021-02-08 18:52:44 -0600

    transcurso del lab

---
+++ lab3/lab3.X/lab03.c
@@ -0,0 +1,127 @@
+/*
+ * File:   lab03.c
+ * Author: betov
+ *
+ * Created on 8 de febrero de 2021, 10:35 AM
+ */
+//******************************************************************************
+//Extraido de: https://electrosome.com/lcd-pic-mplab-xc8/#google_vignette
+//******************************************************************************
+
+//******************************************************************************
+// Palabra de configuración
+//******************************************************************************
+// CONFIG1
+#pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator: Crystal/resonator on RA6/OSC2/CLKOUT and RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF       // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+//******************************************************************************
+// Variables
+//******************************************************************************
+
+//******************************************************************************
+// Importación de librerías
+//******************************************************************************
+#include <xc.h>
+#include "lcd.h";
+#define _XTAL_FREQ 8000000
+#define RS RD2
+#define EN RD3
+#define D4 RD4
+#define D5 RD5
+#define D6 RD6
+#define D7 RD7
+
+//******************************************************************************
+// Prototipos de funciones
+//******************************************************************************
+
+//******************************************************************************
+// Ciclo principal
+//******************************************************************************
+
+    //**************************************************************************
+    // Loop principal
+    //**************************************************************************
+
+//******************************************************************************
+// Configuración
+//******************************************************************************
+
+//******************************************************************************
+// Funciones
+//******************************************************************************
+
+//Parte de la lcd
+// BEGIN CONFIG
+#pragma config FOSC = HS // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF // Watchdog Timer Enable bit (WDT enabled)
+#pragma config PWRTE = OFF // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF // Flash Program Memory Code Protection bit (Code protection off)
+//END CONFIG
+
+int main()
+{
+  unsigned int a;
+  TRISD = 0x00;
+  Lcd_Init();
+  while(1)
+  {
+    Lcd_Clear();
+    Lcd_Set_Cursor(1,1);
+    Lcd_Write_String("LCD Library for");
+    Lcd_Set_Cursor(2,1);
+    Lcd_Write_String("MPLAB XC8");
+    __delay_ms(2000);
+    Lcd_Clear();
+    Lcd_Set_Cursor(1,1);
+    Lcd_Write_String("Developed By");
+    Lcd_Set_Cursor(2,1);
+    Lcd_Write_String("electroSome");
+    __delay_ms(2000);
+    Lcd_Clear();
+    Lcd_Set_Cursor(1,1);
+    Lcd_Write_String("www.electroSome.com");
+
+    for(a=0;a<15;a++)
+    {
+        __delay_ms(300);
+        Lcd_Shift_Left();
+    }
+
+    for(a=0;a<15;a++)
+    {
+        __delay_ms(300);
+        Lcd_Shift_Right();
+    }
+
+    Lcd_Clear();
+    Lcd_Set_Cursor(2,1);
+    Lcd_Write_Char('e');
+    Lcd_Write_Char('S');
+    __delay_ms(2000);
+  }
+  return 0;
+}  
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
---
+++ lab3/lab3.X/lcd.h
@@ -0,0 +1,149 @@
+/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS. 
+ */
+
+/* 
+ * File:   
+ * Author: Helder
+ * Comments:
+ * Revision history: 
+ */
+//******************************************************************************
+//Extraido de: https://electrosome.com/lcd-pic-mplab-xc8/#google_vignette
+//******************************************************************************
+//LCD Functions Developed by electroSome
+#include <xc.h> 
+#include <stdint.h> 
+#include "LCD.h" 
+#define _XTAL_FREQ 4000000 
+
+void Puerto(uint8_t x)
+{
+	if(a & 1)
+		D4 = 1;
+	else
+		D4 = 0;
+
+	if(a & 2)
+		D5 = 1;
+	else
+		D5 = 0;
+
+	if(a & 4)
+		D6 = 1;
+	else
+		D6 = 0;
+
+	if(a & 8)
+		D7 = 1;
+	else
+		D7 = 0;
+}
+void Lcd_Cmd(char a)
+{
+	RS = 0;             // => RS = 0
+	Puerto(a);
+	EN  = 1;             // => E = 1
+        __delay_ms(4);
+        EN  = 0;             // => E = 0
+}
+
+Lcd_Clear()
+{
+	Lcd_Cmd(0);
+	Lcd_Cmd(1);
+}
+
+void Lcd_Set_Cursor(char a, char b)
+{
+	char temp,z,y;
+	if(a == 1)
+	{
+	  temp = 0x80 + b - 1;
+		z = temp>>4;
+		y = temp & 0x0F;
+		Lcd_Cmd(z);
+		Lcd_Cmd(y);
+	}
+	else if(a == 2)
+	{
+		temp = 0xC0 + b - 1;
+		z = temp>>4;
+		y = temp & 0x0F;
+		Lcd_Cmd(z);
+		Lcd_Cmd(y);
+	}
+}
+
+void Lcd_Init()
+{
+  Puerto(0x00);
+   __delay_ms(20);
+  Lcd_Cmd(0x03);
+	__delay_ms(5);
+  Lcd_Cmd(0x03);
+	__delay_ms(11);
+  Lcd_Cmd(0x03);
+  /////////////////////////////////////////////////////
+  Lcd_Cmd(0x02);
+  Lcd_Cmd(0x02);
+  Lcd_Cmd(0x08);
+  Lcd_Cmd(0x00);
+  Lcd_Cmd(0x0C);
+  Lcd_Cmd(0x00);
+  Lcd_Cmd(0x06);
+}
+
+void Lcd_Write_Char(char a)
+{
+   char temp,y;
+   temp = a&0x0F;
+   y = a&0xF0;
+   RS = 1;             // => RS = 1
+   Lcd_Port(y>>4);             //Data transfer
+   EN = 1;
+   __delay_us(40);
+   EN = 0;
+   Lcd_Port(temp);
+   EN = 1;
+   __delay_us(40);
+   EN = 0;
+}
+
+void Lcd_Write_String(char *a)
+{
+	int i;
+	for(i=0;a[i]!='\0';i++)
+	   Lcd_Write_Char(a[i]);
+}
+
+void Lcd_Shift_Right()
+{
+	Lcd_Cmd(0x01);
+	Lcd_Cmd(0x0C);
+}
+
+void Lcd_Shift_Left()
+{
+	Lcd_Cmd(0x01);
+	Lcd_Cmd(0x08);
+}
+
+
