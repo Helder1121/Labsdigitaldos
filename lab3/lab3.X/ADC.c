7888bac5e9fa34f3307f98de01d79e3604e6a3f4
Helder1121 <78274380+Helder1121@users.noreply.github.com>
2021-02-11 17:53:16 -0600

    Libreria de la lcd

---
+++ lab3/lab3.X/ADC.c
@@ -0,0 +1,15 @@
+/*
+ * File:   ADC.c
+ * Author: betov
+ *
+ * Created on 11 de febrero de 2021, 05:35 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
---
+++ lab3/lab3.X/LCD.c
@@ -0,0 +1,100 @@
+//Libreria de Pablo Mazariegos en clase de 4 bits modificada a unos de 8bits
+
+#include <xc.h>
+#include <stdint.h>
+#include "lcd.h"
+#define _XTAL_FREQ 8000000
+
+//Funcion para indicar el caracter segun sea el tamaño del mismo. 
+void Puerto(uint8_t x){
+	if(x & 1){
+		D0 = 1;}
+	else{
+		D0 = 0;}
+    if(x & 2){
+		D1 = 1;}
+	else{
+		D1 = 0;}
+    if(x & 4){
+		D2 = 1;}
+	else{
+		D2 = 0;}
+    if(x & 8){
+		D3 = 1;}
+	else{
+		D3 = 0;}
+    if(x & 16){
+		D4 = 1;}
+	else{
+		D4 = 0;}
+    if(x & 32){
+		D5 = 1;}
+	else{
+		D5 = 0;}
+    if(x & 64){
+		D6 = 1;}
+	else{
+		D6 = 0;}
+    if(x & 128){
+		D7 = 1;}
+	else{
+		D7 = 0;}
+}
+
+//Funcion para imprimir el caracter
+void LCD_CMD(char a){
+    RS = 1;//Las direcciones a los caracteres
+    Puerto(a);
+    EN = 1;//Mandar el valor
+    __delay_us(5);
+    EN = 0;//Verificar si el valor de carac llego
+    __delay_us(5);
+    __delay_us(50);
+}
+//Funcion para mandar los datos a la LCD
+void datosLCD(uint8_t x){
+    RS = 0;//Modifica el contraste de la patalla
+    Puerto(x);
+    EN = 1;//Mandar el valor
+    __delay_us(5);
+    EN = 0;//Verificar si el valor de carac llego
+    __delay_us(5);
+    __delay_ms(2);   
+}
+//Funcion para limpiar la LCD
+void LCD_Limpia(void){
+    datosLCD(0);
+    datosLCD(1);
+}
+//Funcion para iniciar la LCD
+void Lcd_Init(){
+    RS = 0;
+    EN = 0;
+    Puerto(0x00);
+    __delay_ms(50)
+    datosLCD(0x02);
+    datosLCD(0x38);
+    datosLCD(0x0C);
+    datosLCD(0x06);
+}
+
+void Lcd_Set_Cursor(uint8_t x, uint8_t y){
+	uint8_t a;
+	if(x == 1){
+	  a = 0x80 + y;
+		datosLCD(a);
+    }
+	else if(x == 2){
+	  a = 0xC0 + y;
+		datosLCD(a);
+    }
+}
+//Funcion para mandar un string
+void Lcd_Write_String(char *a)
+{
+	int i;
+	for(i=0;a[i]!='\0';i++)
+	   LCD_CMD(a[i]);
+}
+
+
