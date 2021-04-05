7abf2c41b968c9276007071829db0f51a2668829
Helder1121 <78274380+Helder1121@users.noreply.github.com>
2021-04-05 12:46:15 -0600

    comentado

--- Lab5/microsd/microsd.ino
+++ Lab5/microsd/microsd.ino
@@ -3,44 +3,43 @@
 //Helder Ovalle ;18349
 //Seccion:20
 
+//Librerias a utilizar
 #include <SPI.h>
 #include <SD.h>
-
 File root;
 File myFile;
+//Variable para las variantes de las imagenes 
 int imag = 0;
-void setup()
-{
+void setup(){
   // Open serial communications and wait for port to open:
-  Serial.begin(115200);
+  Serial.begin(115200);//Baudios a utilizar 
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
 
-  SPI.setModule(0);
-  Serial.print("Initializing SD card...");
+  SPI.setModule(0);//Configuracion para la inicializacion del SPI
+  Serial.print("Initializing SD card...");//Inciando la SD
   // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
   // Note that even if it's not used as the CS pin, the hardware SS pin
   // (10 on most Arduino boards, 53 on the Mega) must be left as an output
   // or the SD library functions will not work.
-  pinMode(PA_3, OUTPUT);
+  pinMode(PA_3, OUTPUT);//PA_3CS
 
   if (!SD.begin(PA_3)) {
-    Serial.println("initialization failed!");
+    Serial.println("initialization failed!");//Conexion fallida
     return;
   }
-  Serial.println("initialization done.");
-  root = SD.open("/");
-  printDirectory(root, 0);
+  Serial.println("initialization done.");//Conexion exitosa
+  root = SD.open("/");//Abre los archivos 
+  printDirectory(root, 0);//Muestra los archivos (imaganes)
 }
 
-
-void loop()
-{
-if (Serial.available()>0){
-  imag = Serial.read();
+void loop(){
+if (Serial.available()>0){//Valor rque se ingresara en la ventana 
+  imag = Serial.read();//Variable a leer
+  //Primera imagen
   if (imag == '1'){
-    myFile = SD.open("darkB.txt"); 
+    myFile = SD.open("darkB.txt");//Nombre del archivo .txt
     if (myFile){
       while (myFile.available()){
         Serial.write(myFile.read());
@@ -48,12 +47,13 @@ if (Serial.available()>0){
       myFile.close();
     }
     else{
-      Serial.println("error al abrir el .txt");
+      Serial.println("error al abrir el .txt");//Mensaje de error
     }
   }
 }
+  //Segunda imagen
   if (imag == '2'){
-    myFile = SD.open("mario.txt"); 
+    myFile = SD.open("mario.txt"); //Nombre del archivo .txt
     if (myFile){
       while (myFile.available()){
         Serial.write(myFile.read());
@@ -61,11 +61,12 @@ if (Serial.available()>0){
       myFile.close();
     }
     else{
-      Serial.println("error al abrir el .txt");
+      Serial.println("error al abrir el .txt");//Mensaje de error
     }
   }
+  //Tercera imagen
   if (imag == '3'){
-    myFile = SD.open("wason.txt"); 
+    myFile = SD.open("wason.txt"); //Nombre del archivo .txt
     if (myFile){
       while (myFile.available()){
         Serial.write(myFile.read());
@@ -73,10 +74,11 @@ if (Serial.available()>0){
       myFile.close();
     }
     else{
-      Serial.println("error al abrir el .txt");
+      Serial.println("error al abrir el .txt");//Mensaje de error
     }
   }
 }
+//La funcion para enseñar los archivos SD
 void printDirectory(File dir, int numTabs) {
    while(true) {
      File entry =  dir.openNextFile();
